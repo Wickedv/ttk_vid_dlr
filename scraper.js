8b748e80293d089490c1ad2ce264acca36d1ca51
Manish <coish5insped@gmail.com>
2024-03-12 23:15:26 -0400

    Refactor MongoDB connection and data insertion

--- scraper.js
+++ scraper.js
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 import { config } from "dotenv";
 // import connectDB from "./connect.js";
 import mongoose from "mongoose";
-import {MongoClient} from 'mongodb';
+import { MongoClient } from "mongodb";
 import { runInContext } from "vm";
 
 config();
@@ -12,63 +12,73 @@ const proxyList = ["proxy1-ip:port", "proxy2-ip:port", "proxy3-ip:port"];
 const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
 //,`--proxy-server=${randomProxy}`
 
-
+// console.log(process.env.mondodb_uri)
 
 (async () => {
-    const client = mongoose.connect(process.env.mongodb_uri)
-    .then (() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(err));
+  try {
+    const client = await mongoose
+      .connect(process.env.mondodb_uri)
+      .then(() => console.log("Connected to MongoDB"));
     const mydata = new mongoose.Schema({
-        src: {type:String, required:true},
-        alt: {type:String, required:true},
-        img: {type:String, required:true}
+      src: { type: String, required: true },
+      alt: { type: String, required: true },
+      img: { type: String, required: true },
     });
 
-    const dat = mongoose.model('mydata', mydata);
+    const dat = mongoose.model("mydata", mydata);
 
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      defaultViewport: null,
+      userDataDir: "./myUserDataDir",
+    });
+    const page = await browser.newPage();
 
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    defaultViewport: null,
-    userDataDir: "./myUserDataDir",
-  });
-  const page = await browser.newPage();
+    await page.goto(process.env.URL);
+    await page.waitForSelector(".css-1as5cen-DivWrapper"); //this the download button
 
-  await page.goto(process.env.URL);
-  await page.waitForSelector(".css-1as5cen-DivWrapper"); //this the download button
+    await page.evaluate(async () => {
+      for (let i = 0; i < 10; i++) {
+        window.scrollTo(0, document.body.scrollHeight);
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      }
+    });
 
-  await page.evaluate(async () => {
-    for (let i = 0; i < 10; i++) {
-      window.scrollTo(0, document.body.scrollHeight);
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+    const lnks = await page.$$eval(".css-1as5cen-DivWrapper > a", (el) =>
+      el.map((x) => x.getAttribute("href"))
+    );
+    const imgData = await page.$$eval(".css-41hm0z  img", (images) =>
+      images.map((img) => ({
+        src: img.getAttribute("src"),
+        alt: img.getAttribute("alt"),
+      }))
+    );
+
+    const mergeData = imgData.map((item, index) => ({
+      link: lnks[index],
+      ...item,
+    }));
+
+    await dat.deleteMany({});
+
+    for (let item of mergeData) {
+      let data = new dat({
+        src: item.link,
+        img: item.src,
+        alt: item.alt,
+      });
+
+      await data.save();
     }
-  });
 
-  const lnks = await page.$$eval(".css-1as5cen-DivWrapper > a", (el) =>
-    el.map((x) => x.getAttribute("href"))
-  );
-  const imgData = await page.$$eval(".css-41hm0z  img", (images) =>
-    images.map((img) => ({
-      src: img.getAttribute("src"),
-      alt: img.getAttribute("alt"),
-    }))
-  );
+    //mydata.insertMany(imgData);
+    // console.log(imgData);
 
-  for (let item of imgData) {
-    let data = new dat({
-      src: item.src,
-      alt: item.alt,
-      img: "img",
-    });
-    await data.save();
+    const data = JSON.stringify(imgData);
+    fs.writeFile("links.txt", data);
+  } finally {
+    mongoose.disconnect();
+    browser.close();
   }
-
-  mydata.insertMany(imgData);
-  // console.log(imgData);
-
-  const data = JSON.stringify(imgData);
-  fs.writeFile("links.txt", data);
-
-  browser.close();
 })();
