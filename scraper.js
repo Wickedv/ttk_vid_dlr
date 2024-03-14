2622f0d96c2f0a1eca57ba857f2233b058326ab8
Manish <coish5insped@gmail.com>
2024-03-13 23:55:04 -0400

    Refactor scraper.js and add pickedVideos.json to .gitignore

--- scraper.js
+++ scraper.js
@@ -1,24 +1,143 @@
+// import puppeteer from "puppeteer";
+// import { promises as fs } from "fs";
+// import { config } from "dotenv";
+// // import connectDB from "./connect.js";
+// import mongoose from "mongoose";
+// import { MongoClient } from "mongodb";
+// import { runInContext } from "vm";
+
+// config();
+
+// const proxyList = ["proxy1-ip:port", "proxy2-ip:port", "proxy3-ip:port"];
+// const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
+// //,`--proxy-server=${randomProxy}`
+
+// // console.log(process.env.mondodb_uri)
+
+// (async () => {
+//   try {
+//     const client = await mongoose
+//       .connect(process.env.mondodb_uri)
+//       .then(() => console.log("Connected to MongoDB"));
+//     const mydata = new mongoose.Schema({
+//       src: { type: String, required: true },
+//       alt: { type: String, required: true },
+//       img: { type: String, required: true },
+//     });
+
+//     const dat = mongoose.model("mydata", mydata);
+
+//     const browser = await puppeteer.launch({
+//       headless: false,
+//       args: ["--no-sandbox", "--disable-setuid-sandbox"],
+//       defaultViewport: null,
+//       userDataDir: "./myUserDataDir",
+//     });
+//     const page = await browser.newPage();
+
+//     await page.goto(process.env.URL);
+//     await page.waitForSelector(".css-1as5cen-DivWrapper"); //this the download button
+
+//     await page.evaluate(async () => {
+//       for (let i = 0; i < 10; i++) {
+//         window.scrollTo(0, document.body.scrollHeight);
+//         await new Promise((resolve) => setTimeout(resolve, 2000));
+//       }
+//     });
+
+//     const lnks = await page.$$eval(".css-1as5cen-DivWrapper > a", (el) =>
+//       el.map((x) => x.getAttribute("href"))
+//     );
+//     const imgData = await page.$$eval(".css-41hm0z  img", (images) =>
+//       images.map((img) => ({
+//         src: img.getAttribute("src"),
+//         alt: img.getAttribute("alt"),
+//       }))
+//     );
+
+//     const mergeData = imgData.map((item, index) => ({
+//       link: lnks[index],
+//       ...item,
+//     }));
+
+//     fs.writeFile("links.txt", JSON.stringify(mergeData));
+
+//     const localData =  JSON.parse(fs.readFileSync("links.txt", "utf-8"));
+
+//     await dat.deleteMany({});
+
+//     for (let item of mergeData) {
+//       let data = new dat({
+//         src: item.link,
+//         img: item.src,
+//         alt: item.alt,
+//       });
+
+//       await data.save();
+//     }
+
+//     //mydata.insertMany(imgData);
+//     // console.log(imgData);
+
+//     const data = JSON.stringify(imgData);
+//     fs.writeFile("links.txt", data);
+//   } finally {
+//     mongoose.disconnect();
+//     browser.close();
+//   }
+// })();
+
 import puppeteer from "puppeteer";
 import { promises as fs } from "fs";
 import { config } from "dotenv";
-// import connectDB from "./connect.js";
 import mongoose from "mongoose";
-import { MongoClient } from "mongodb";
-import { runInContext } from "vm";
 
 config();
 
 const proxyList = ["proxy1-ip:port", "proxy2-ip:port", "proxy3-ip:port"];
 const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
-//,`--proxy-server=${randomProxy}`
+let browser;
 
-// console.log(process.env.mondodb_uri)
+// Queue to store the history of picked videos
+let pickedVideos = [];
+// Load the history of picked videos from a file
+try {
+  pickedVideos = JSON.parse(fs.readFileSync('pickedVideos.json', 'utf-8'));
+} catch (err) {
+  // If the file does not exist, initialize pickedVideos as an empty array
+  pickedVideos = [];
+}
+
+async function pickRandomVideo(dat) {
+  // Get the total number of videos
+  const count = await dat.countDocuments().exec();
+
+  // Pick a random video
+  let video;
+  do {
+    const random = Math.floor(Math.random() * count);
+    video = await dat.findOne().skip(random).exec();
+  } while (pickedVideos.includes(video._id.toString()));
+
+  // Add the picked video to the queue
+  pickedVideos.push(video._id.toString());
+
+  // If the queue's size exceeds 7, remove the oldest video from the queue
+  if (pickedVideos.length > 7) {
+    pickedVideos.shift();
+  }
+
+// Save the history of picked videos to a file
+  await fs.writeFile('pickedVideos.json', JSON.stringify(pickedVideos));
+  
+  return video;
+}
 
 (async () => {
   try {
-    const client = await mongoose
-      .connect(process.env.mondodb_uri)
-      .then(() => console.log("Connected to MongoDB"));
+    await mongoose.connect(process.env.mondodb_uri);
+    console.log("Connected to MongoDB");
+
     const mydata = new mongoose.Schema({
       src: { type: String, required: true },
       alt: { type: String, required: true },
@@ -27,7 +146,7 @@ const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
 
     const dat = mongoose.model("mydata", mydata);
 
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
       headless: false,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
       defaultViewport: null,
@@ -36,7 +155,7 @@ const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
     const page = await browser.newPage();
 
     await page.goto(process.env.URL);
-    await page.waitForSelector(".css-1as5cen-DivWrapper"); //this the download button
+    await page.waitForSelector(".css-1as5cen-DivWrapper");
 
     await page.evaluate(async () => {
       for (let i = 0; i < 10; i++) {
@@ -60,23 +179,27 @@ const randomProxy = proxyList[Math.floor(Math.random() * proxyList.length)];
       ...item,
     }));
 
-    await dat.deleteMany({});
+    await fs.writeFile("links.txt", JSON.stringify(mergeData));
 
-    for (let item of mergeData) {
-      let data = new dat({
-        src: item.link,
-        img: item.src,
-        alt: item.alt,
-      });
+    const localData = JSON.parse(await fs.readFile("links.txt", "utf-8"));
 
-      await data.save();
+    // Get MongoDB data
+    const mongoData = await dat.find({}).exec();
+
+    // Compare data and find differences
+    const differences = localData.filter(
+      (item) => !mongoData.some((mongoItem) => mongoItem.src === item.link)
+    );
+
+    // Upload differences to MongoDB
+    if (differences.length > 0) {
+      await dat.insertMany(differences);
+      console.log("Number of documents inserted: " + differences.length);
+    } else {
+      console.log("No new data to insert.");
     }
-
-    //mydata.insertMany(imgData);
-    // console.log(imgData);
-
-    const data = JSON.stringify(imgData);
-    fs.writeFile("links.txt", data);
+    const video = await pickRandomVideo(dat);
+    console.log(video);
   } finally {
     mongoose.disconnect();
     browser.close();
